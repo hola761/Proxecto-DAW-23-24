32046e0debf2e9f7607cbc3a7b8625940441a39f
sandra <sandrasm.job5@gmail.com>
2024-12-01 13:20:54 +0100

    archivos antigua api php eliminados

--- src/api/PHP/crearCliente.php
+++
@@ -1,5 +0,0 @@
-<?php
-    require_once("/controlador/ClienteControlador.php");
-
-    ClienteControlador::crearCliente("87495123p", "Borrar", "981445236", "borrar@mail.com", "usuBorrar", "usuB123.");
-?>
