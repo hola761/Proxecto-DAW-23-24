719f42d8b5afc80cd65c0499eab03271f076ec1c
sandra <sandrasm.job5@gmail.com>
2024-12-07 19:07:59 +0100

    empezando listado clientes js

---
+++ src/front/assets/js/listadoClientes.js
@@ -0,0 +1,28 @@
+const $d = document,
+  $contenedorClientesM = $d.querySelector("#contenido_tabla-movil");
+
+function ajax(options) {
+  let { url, method, fSuccess, fError, data } = options;
+  fetch(url, {
+    method: method || "GET",
+    headers: { "Content-type": "application/json; charset=utf-8" },
+    body: JSON.stringify(data),
+  })
+    .then((resp) => (resp.ok ? resp.json() : Promise.reject(resp)))
+    .then((json) => fSuccess(json))
+    .catch((err) => {
+      let error = {
+        status: err.status,
+        statusText: err.statusText || "Ocurrió un error",
+      };
+      fError(error);
+    });
+}
+
+function recuperarClientes(){
+    ajax({
+        url: 'http://localhost:8095/usuarios',
+        fSuccess: json=>console.log(json),
+        fError: error=>console.log(error)  
+    })
+}
