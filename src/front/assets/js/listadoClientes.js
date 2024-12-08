9407f12d4769b29a84e9baaa1fd657e12beedf89
hola761 <sandrasm.job5@gmail.com>
2024-12-08 19:26:01 +0100

    Viendo los datos de los clientes por primera vez

--- src/front/assets/js/listadoClientes.js
+++ src/front/assets/js/listadoClientes.js
@@ -1,5 +1,7 @@
 const $d = document,
-  $contenedorClientesM = $d.querySelector("#contenido_tabla-movil");
+  $contenedorClientesM = $d.getElementById('contenido-tabla-movil')
+
+let usuarios = []
 
 function ajax(options) {
   let { url, method, fSuccess, fError, data } = options;
@@ -19,12 +21,27 @@ function ajax(options) {
     });
 }
 
-function recuperarClientes(){
-    ajax({
-        url: 'http://localhost:8095/usuarios',
-        fSuccess: json=>console.log(json),
-        fError: error=>console.log(error)  
-    })
+function recuperarClientes() {
+  ajax({
+    url: 'http://localhost:8095/usuarios',
+    // fSuccess: json=>console.log(json),
+    fSuccess: users => {
+      usuarios = users._embedded.usuarios
+      cargarUsuarios(usuarios)
+    },
+    fError: error => console.log(error)
+  })
 }
 
+function cargarUsuarios(usuarios) {
+  $contenedorClientesM.innerHTML = usuarios.map(usuario =>
+    `
+      <tr>
+        <td>${usuario.dni}</td>
+        <td>${usuario.nombre}</td>
+        <td>${usuario.telefono}</td>
+        <td><a href="#">Ver</a></td>
+      </tr>
+    `).join('')
+}
 $d.addEventListener('DOMContentLoaded', recuperarClientes)
